64d0ddf63519a5706d497cd2b3f6dbd6a2b8804f
Rolando Bernal <jrolandobernal@gmail.com>
2015-05-13 20:23:10 -0500

    Completed loading the Home page and Fixed the link to the Auction page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Savings Multiplied | version 3' });
 });
+
+/* GET auction page. */
+ router.get('/auction', function(req, res, next) {
+   res.render('auction', { title: 'Auction | Savings Multiplied' });
+ });
 
 module.exports = router;
